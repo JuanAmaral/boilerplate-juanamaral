1523fb22a5013ffb5a618bbac44fd0901ae8dfa9
JuanAmaral <trabalho@juanamaral.com.br>
2022-03-23 09:52:37 -0300

    First-commit

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { MainContent } from './HomeStyle';
+
+export default function Home() {
+  return (
+    <MainContent>
+      <div style={{ width: '10rem', height: '10rem', background: 'blue' }}>
+        Jogo
+      </div>
+    </MainContent>
+  );
+}
